ba1bb7693c1a815885298a1a36d436977d701a26
BL506 <blakel4002@gmail.com>
2025-01-28 15:48:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+from board import show_game
+from board import random_move
+
 from neural_network import TTTNet
 from neural_network import random_train
+from neural_network import nn_move
 
 network = TTTNet()
 
 print("Training network vs random")
-random_train(network=network)
+random_train(network)
 print("Training complete")
 
+nn_player = nn_move(network)
+count = 0
+
+while count < 10:
+  show_game(nn_player, random_move)
+  count += 1
