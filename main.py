346f79b886b0055bacad986113caa85e0f14e3d4
BL506 <blakel4002@gmail.com>
2025-01-28 15:05:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from board import show_game
-from board import random_move
-
 from neural_network import TTTNet
 from neural_network import random_train
 
@@ -9,5 +6,4 @@ network = TTTNet()
 print("Training network vs random")
 random_train(network=network)
 print("Training complete")
-
 
